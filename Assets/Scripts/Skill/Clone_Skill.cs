03e3ecb64596654c5b402af7c2c47a6b289fa1ea
NhamMatOTho <Hoangtrongluoi@gmail.com>
2024-06-16 21:50:53 +0700

    Update skill tree
    Update clone skill and crystal skill

--- Assets/Scripts/Skill/Clone_Skill.cs
+++ Assets/Scripts/Skill/Clone_Skill.cs
@@ -11,10 +11,44 @@ public class Clone_Skill : Skill
     [SerializeField] private float cloneDuration;
     [SerializeField] private bool canAttack;
 
+    [SerializeField] private bool createCloneOnDashStart;
+    [SerializeField] private bool createCloneOnDashOver;
+    [SerializeField] private bool canCreateCloneOnCounterAttack;
+
     public void CreateClone(Transform _clonePosition, Vector3 _offset)
     {
         GameObject newClone = Instantiate(clonePrefab);
 
         newClone.GetComponent<Clone_Skill_Controller>().SetupClone(_clonePosition, cloneDuration, canAttack, _offset, FindClosestEnemy(newClone.transform));
+    }
+
+    public void CreateCloneOnDashStart()
+    {
+        if (createCloneOnDashStart)
+        {
+            CreateClone(player.transform, Vector3.zero);
+        }
+    }
+
+    public void CreateCloneOnDashOver()
+    {
+        if (createCloneOnDashOver)
+        {
+            CreateClone(player.transform, Vector3.zero);
+        }
+    }
+
+    public void createCloneOnCounterAttack(Transform _enemyTranform)
+    {
+        if (canCreateCloneOnCounterAttack)
+        {
+            StartCoroutine(CreateCloneWithDelay(_enemyTranform, new Vector3(2 * player.facingDir, 0)));
+        }
+    }
+
+    private IEnumerator CreateCloneWithDelay(Transform _tranform, Vector3 _offset)
+    {
+        yield return new WaitForSeconds(0.4f);
+        CreateClone(_tranform, _offset);
     }
 }
